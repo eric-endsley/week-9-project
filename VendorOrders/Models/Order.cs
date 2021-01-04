93fa25c1b05438c514078a34986fe565e21198c4
Eric Endsley <eric.endsley4@gmail.com>
2021-01-03 21:42:00 -0800

    write properties for Order class

--- VendorOrders/Models/Order.cs
+++ VendorOrders/Models/Order.cs
@@ -4,6 +4,13 @@ namespace VendorOrders.Models
 {
     public class Order
     {
-        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public int Price { get; private set; }
+        public int Id { get; }
+        private static List<Order> _instances = new List<Order> { };
     }
+
+    Order() 
 }
