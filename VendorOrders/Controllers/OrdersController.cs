8f24e99735cd27ccb06389f27f11c1145f7b2148
Eric Endsley <eric.endsley4@gmail.com>
2021-01-04 04:37:57 -0800

    add show route to OrdersController

--- VendorOrders/Controllers/OrdersController.cs
+++ VendorOrders/Controllers/OrdersController.cs
@@ -13,5 +13,17 @@ namespace VendorOrders.Controllers
             Vendor vendor = Vendor.Find(vendorId);
             return View(vendor);
         }
+
+        [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+        public ActionResult Show(int vendorId, int orderId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Order currentOrder = Order.Find(orderId);
+            Vendor currentVendor = Vendor.Find(vendorId);
+            model.Add("vendor", currentVendor);
+            model.Add("order", currentOrder);
+            return View(model);
+        }
+
     }
 }
