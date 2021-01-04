97fd79fd441473995bd82d8a5c13eb538b370b91
Eric Endsley <eric.endsley4@gmail.com>
2021-01-04 03:57:42 -0800

    add New route to OrdersController

--- VendorOrders/Controllers/OrdersController.cs
+++ VendorOrders/Controllers/OrdersController.cs
@@ -4,5 +4,9 @@ namespace VendorOrders.Controllers
 {
     public class OrdersController : Controller
     {
+        [HttpGet("/vendors/{vendorId}/orders/new")]
+        public ActionResult New(int vendorId)
+        Vendor vendor = Vendor.Find(vendorId);
+        return View(vendor)
     }
 }
