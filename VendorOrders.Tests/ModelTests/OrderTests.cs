33c7e75ba7d4a7ce0bc45fae3d0721e2823e9352
Eric Endsley <eric.endsley4@gmail.com>
2021-01-03 21:19:17 -0800

    write first test of Order constructor

--- VendorOrders.Tests/ModelTests/OrderTests.cs
+++ VendorOrders.Tests/ModelTests/OrderTests.cs
@@ -6,11 +6,18 @@ using System;
 namespace VendorOrders.Tests
 {
     [TestClass]
-    public class VendorTests : IDisposable
+    public class OrderTests : IDisposable
     {
         public void Dispose()
         {
             Vendor.ClearAll();
+        }
+
+        [TestMethod]
+        OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order();
+            Assert.AreEqual(typeof(Order), newItem.GetType())
         }
         
     }
